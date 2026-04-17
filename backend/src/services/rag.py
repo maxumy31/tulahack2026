814ef6dfd9892909a5482c820d9f5ffc93f64b2a
tima <tidmkhokhlov@edu.hse.ru>
2026-04-18 02:10:39 +0300

    upd(backend): logger, cors

--- backend/src/services/rag.py
+++ backend/src/services/rag.py
@@ -3,7 +3,7 @@ from src.schemas.chat import ChatRequest
 
 class Rag:
     @classmethod
-    async def get_answer(cls, request: ChatRequest):
+    async def get_answer(cls, request: str):
         await asyncio.sleep(1)
         print("Обрабатываю запрос: ", request)
         result = f"Вот 5 наиболее подходящих чанков"
